4ffa7fb415a0d3f081bbe2e30bdf189b7008b5b5
Sergey Ermakovich <s.ermakovich@gmail.com>
2011-12-14 17:18:55 +0300

    DEV: RecurlyPlan.GetAll

---
+++ Test/PlanTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Recurly;
+using NUnit.Framework;
+
+namespace Recurly.Test
+{
+    [TestFixture]
+    public class PlanTest
+    {
+        [Test]
+        public void GetAll()
+        {
+            IEnumerable<RecurlyPlan> plans = RecurlyPlan.GetAll();
+
+            Assert.NotNull(plans);
+        }
+    }
+}
